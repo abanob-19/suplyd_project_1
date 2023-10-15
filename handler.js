c87ffb4640befa780be5bedb8ce004b0da45f960
abanob-19 <abanob11@gmail.com>
2023-10-15 15:33:35 +0300

    Initial commit

---
+++ handler.js
@@ -0,0 +1,11 @@
+    // handler.js
+    'use strict';
+
+    module.exports.hello = async (event) => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+        message: 'Hello, world!',
+        }),
+    };
+    };
